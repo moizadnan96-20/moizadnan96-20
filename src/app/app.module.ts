2f58180ecbf1a719d588ffc8e54a86ea008044c3
mirzarizwan2 <muhammad.rizwan@perception-it.com>
2021-03-16 15:52:30 +0500

    service, clients

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+import { HeroComponent } from './components/content/hero/hero.component';
+import { FeaturedComponent } from './components/content/featured/featured.component';
+import { AboutComponent } from './components/content/about/about.component';
+import { ServiceSectionComponent } from './components/content/service-section/service-section.component';
+import { CtaSectionComponent } from './components/content/cta-section/cta-section.component';
+import { TeamSectionComponent } from './components/content/team-section/team-section.component';
+import { ClientsComponent } from './components/content/clients/clients.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    HeroComponent,
+    FeaturedComponent,
+    AboutComponent,
+    ServiceSectionComponent,
+    CtaSectionComponent,
+    TeamSectionComponent,
+    ClientsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
