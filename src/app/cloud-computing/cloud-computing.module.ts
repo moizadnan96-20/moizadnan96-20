1f44dd25e0856c8bc91bfcdabbbf1045a72f9eb3
mirzarizwan2 <muhammad.rizwan@perception-it.com>
2021-03-29 10:49:34 +0500

    cloud computing

--- src/app/cloud-computing/cloud-computing.module.ts
+++ src/app/cloud-computing/cloud-computing.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CloudComputingRoutingModule } from './cloud-computing-routing.module';
 import { CloudComputingComponent } from './components/cloud-computing/cloud-computing.component';
+import { HeroComponent } from './components/cloud-computing/hero/hero.component';
+import { InfoSectionComponent } from './components/cloud-computing/info-section/info-section.component';
+import { QuestionSectionComponent } from './components/cloud-computing/question-section/question-section.component';
+import { TestimonialSectionComponent } from './components/cloud-computing/testimonial-section/testimonial-section.component';
+import { FooterSectionComponent } from './components/cloud-computing/footer-section/footer-section.component';
+
 
 
 @NgModule({
-  declarations: [CloudComputingComponent],
+  declarations: [CloudComputingComponent, HeroComponent, InfoSectionComponent, QuestionSectionComponent, TestimonialSectionComponent, FooterSectionComponent, ],
   imports: [
     CommonModule,
     CloudComputingRoutingModule
