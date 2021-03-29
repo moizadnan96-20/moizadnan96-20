ba71bf3f59a7471f61dc5afe884f4b2d5457325a
mirzarizwan2 <muhammad.rizwan@perception-it.com>
2021-03-29 16:48:23 +0500

    support-infra manged-services IT-Consultancy cloud-computing fixing

--- src/app/manage-services/manage-services.module.ts
+++ src/app/manage-services/manage-services.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ManageServicesRoutingModule } from './manage-services-routing.module';
 import { ManageServicesComponent } from './components/manage-services/manage-services.component';
+import { HeroComponent } from './components/manage-services/hero/hero.component';
+import { InfoSectionComponent } from './components/manage-services/info-section/info-section.component';
+import { QuestionSectionComponent } from './components/manage-services/question-section/question-section.component';
+import { TestimonialSectionComponent } from './components/manage-services/testimonial-section/testimonial-section.component';
+import { FooterSectionComponent } from './components/manage-services/footer-section/footer-section.component';
 
 
 @NgModule({
-  declarations: [ManageServicesComponent],
+  declarations: [ManageServicesComponent, HeroComponent, InfoSectionComponent, QuestionSectionComponent, TestimonialSectionComponent, FooterSectionComponent, ],
   imports: [
     CommonModule,
     ManageServicesRoutingModule
