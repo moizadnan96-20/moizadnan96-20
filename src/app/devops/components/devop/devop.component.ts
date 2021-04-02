2af1aace0d9c860a984fdddf03ba047d5d1a5e18
Ammad Javed <ammad.javed@perceptionit.com>
2021-04-02 18:53:38 +0500

    sub header sticky

--- src/app/devops/components/devop/devop.component.ts
+++ src/app/devops/components/devop/devop.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-devop',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevopComponent implements OnInit {
 
+  header:any;
+  headerOfset:any;
+
+  @HostListener('window:scroll', ['$event']) 
+  onScrollEvent($event:any){
+    if(window.pageYOffset+90 > this.headerOfset){
+      this.header.classList.add("sticky");
+    } else {
+      this.header.classList.remove("sticky");
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.header=document.getElementById("links-tab");
+  this.headerOfset=this.header?.offsetTop;
   }
 
 }
