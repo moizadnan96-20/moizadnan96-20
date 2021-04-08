d21c214d64199faab2c0e01ab6caa72655285d71
Ammad Javed <ammad.javed@perceptionit.com>
2021-04-08 17:06:27 +0500

    devops

--- src/app/devops/components/devop/devop.component.ts
+++ src/app/devops/components/devop/devop.component.ts
@@ -14,7 +14,14 @@ export class DevopComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event']) 
   onScrollEvent($event:any){
-    if(window.pageYOffset+86 > this.headerOfset){
+    let a;
+    if(window.innerWidth<1160){
+      a=67;
+    }else{
+      a=107;
+    }
+    console.log(window.innerWidth)
+    if(window.pageYOffset+a > this.headerOfset){
       this.header.classList.add("sticky");
     } else {
       this.header.classList.remove("sticky");
