1b8105993d8b75c25fd77afeb854d7e1032a0fe4
Ammad Javed <ammad.javed@perceptionit.com>
2021-04-08 20:26:40 +0500

    sticky header

--- src/app/consultancy/components/consultancy/hero-section/hero-section.component.ts
+++ src/app/consultancy/components/consultancy/hero-section/hero-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSectionComponent implements OnInit {
 
+  header: any;
+  headerOfset: any;
+
+  @HostListener('window:scroll', ['$event'])
+  onScrollEvent($event: any) {
+    let a;
+    if (window.innerWidth < 1160) {
+      a = 67;
+    } else {
+      a = 107;
+    }
+    if (window.pageYOffset + a > this.headerOfset) {
+      this.header.classList.add("sticky");
+    } else {
+      this.header.classList.remove("sticky");
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.header = document.getElementById("links-tab");
+    this.headerOfset = this.header?.offsetTop;
   }
 
 }
