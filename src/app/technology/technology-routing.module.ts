06262f540b708f69b7deb21385987937746b639f
HanzalaMehmoodButt <hanzala.butt24@gmail.com>
2021-04-13 11:27:15 +0500

    Technology module

---
+++ src/app/technology/technology-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TechnologyComponent } from './components/technology/technology.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:TechnologyComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TechnologyRoutingModule { }
