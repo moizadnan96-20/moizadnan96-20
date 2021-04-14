e047b653b26d5def29036ab9e8a94a4b566b87b6
Ammad Javed <ammad.javed@perceptionit.com>
2021-04-14 10:48:47 +0500

    scroller

--- src/app/devops/components/devop/devop.component.ts
+++ src/app/devops/components/devop/devop.component.ts
@@ -9,33 +9,50 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class DevopComponent implements OnInit {
 
-  header:any;
-  headerOfset:any;
+  header: any;
+  headerOfset: any;
+  sticky:boolean=false;
 
-  @HostListener('window:scroll', ['$event']) 
-  onScrollEvent($event:any){
+  @HostListener('window:scroll', ['$event'])
+  onScrollEvent($event: any) {
     let a;
-    if(window.innerWidth<1160){
-      a=67;
-    }else{
-      a=107;
+    if (window.innerWidth < 1160) {
+      a = 67;
+    } else {
+      a = 107;
     }
-    if(window.pageYOffset+a > this.headerOfset){
+    if (window.pageYOffset + a > this.headerOfset) {
       this.header.classList.add("sticky");
+      this.sticky=true;
     } else {
       this.header.classList.remove("sticky");
+      this.sticky=false;
     }
   }
 
   constructor() { }
 
   ngOnInit(): void {
-    this.header=document.getElementById("links-tab");
-  this.headerOfset=this.header?.offsetTop;
+    this.header = document.getElementById("links-tab");
+    this.headerOfset = this.header?.offsetTop;
   }
 
   scroll(el: HTMLElement) {
-    el.scrollIntoView(false);
-}
-
+    el.scrollIntoView();
+    setTimeout(()=>{if (window.innerWidth < 1160) {
+      if(this.sticky){
+        window.scrollBy(0, -100);
+      }else{
+        window.scrollBy(0, -130);
+      }
+    } else {
+      if(this.sticky){
+        window.scrollBy(0, -130);
+      }else{
+    window.scrollBy(0, -170);
+      }
+      
+    }},500)
+    
+  }
 }
