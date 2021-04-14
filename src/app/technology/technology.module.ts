b921070d6bcfffd5bc1ad005e54fb15a4dc5add6
mirzarizwan2 <muhammad.rizwan@perception-it.com>
2021-04-14 13:49:39 +0500

    partners fxing

--- src/app/technology/technology.module.ts
+++ src/app/technology/technology.module.ts
@@ -6,11 +6,12 @@ import { TechnologyComponent } from './components/technology/technology.componen
 import { InfoSectionComponent } from './components/technology/info-section/info-section.component';
 import { SubInfoSectionComponent } from './components/technology/sub-info-section/sub-info-section.component';
 import { QuestionSectionComponent } from './components/technology/question-section/question-section.component';
+import { FooterComponent } from './components/technology/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [TechnologyComponent, InfoSectionComponent, SubInfoSectionComponent, QuestionSectionComponent, ],
+  declarations: [TechnologyComponent, InfoSectionComponent, SubInfoSectionComponent, QuestionSectionComponent, FooterComponent, ],
   imports: [
     CommonModule,
     TechnologyRoutingModule
