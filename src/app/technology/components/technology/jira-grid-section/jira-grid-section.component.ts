e7e9327a1ce751d6330cf5b7026e2c271516d9cc
mirzarizwan2 <muhammad.rizwan@perception-it.com>
2021-04-15 19:14:14 +0500

    Teachnology sub-pages layout and content, managed service page fixing

---
+++ src/app/technology/components/technology/jira-grid-section/jira-grid-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jira-grid-section',
+  templateUrl: './jira-grid-section.component.html',
+  styleUrls: ['./jira-grid-section.component.css']
+})
+export class JiraGridSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/technology/technology.module.ts
+++ src/app/technology/technology.module.ts
@@ -9,11 +9,13 @@ import { SubInfoSectionComponent } from './components/technology/sub-info-sectio
 import { QuestionSectionComponent } from './components/technology/question-section/question-section.component';
 import { FooterComponent } from './components/technology/footer/footer.component';
 import { TechnologyGirdSystemComponent } from './components/technology/technology-gird-system/technology-gird-system.component';
+import { JiraGridSectionComponent } from './components/technology/jira-grid-section/jira-grid-section.component';
+import { IbmGirdSectionComponent } from './components/technology/ibm-gird-section/ibm-gird-section.component';
 
 
 
 @NgModule({
-  declarations: [TechnologyComponent, InfoSectionComponent, SubInfoSectionComponent, QuestionSectionComponent, FooterComponent, HeroSectionComponent, TechnologyGirdSystemComponent ],
+  declarations: [TechnologyComponent, InfoSectionComponent, SubInfoSectionComponent, QuestionSectionComponent, FooterComponent, HeroSectionComponent, TechnologyGirdSystemComponent, JiraGridSectionComponent, IbmGirdSectionComponent ],
   imports: [
     CommonModule,
     TechnologyRoutingModule
