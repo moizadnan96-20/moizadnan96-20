8cc737b6cfeb34092b432f32550612f780895512
mirzarizwan2 <testing.qa96@gmail.com>
2021-06-25 18:39:29 +0500

    update carsole images

--- src/app/landing/info-section/info-section.component.ts
+++ src/app/landing/info-section/info-section.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class InfoSectionComponent implements OnInit {
   // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  images: any = ['../../../assets/Dia/DevOps.svg', '../../../assets/img/project-1.svg', '../../../assets/img/project-2.svg', '../../../assets/img/project-3.svg'
+  images: any = ['../../../assets/carsole/1056.jpg', '../../../assets/carsole/17927.jpg', '../../../assets/carsole/2911473.jpg', '../../../assets/carsole/34.jpg'
 
   ]
 
