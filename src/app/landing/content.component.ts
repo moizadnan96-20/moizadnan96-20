4802e27f5edaded6b2571d6f08a90066a3854e87
moizadnan96-20 <moizadnan96@gmail.com>
2021-06-28 19:33:03 +0500

    changing update

--- src/app/landing/content.component.ts
+++ src/app/landing/content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component,  HostListener,  Inject,  OnInit,  } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
+  windowScrolled!: boolean;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } 
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+ 
 
   ngOnInit(): void {
   }
-
+ 
+  scrollToTop() {
+    (function smoothscroll() {
+        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+          
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    })();
 }
+
+  }
+
---
+++ src/app/technology-updates/components/technology-questions/technology-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technology-questions',
+  templateUrl: './technology-questions.component.html',
+  styleUrls: ['./technology-questions.component.css']
+})
+export class TechnologyQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
