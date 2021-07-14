bd011331e15aaf173f50bfb8328b3747af32c200
Muhammad Rizwan <muhammad.rizwan@perceptionit.com>
2021-07-14 17:49:41 +0500

    update carsole in mobile view

--- src/app/contact-us/contact.service.ts
+++ src/app/contact-us/contact.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ContactService {
-  baseURL = 'http://localhost:3000/api/mail';
+  // baseURL = 'http://localhost:3000/api/mail';
+  baseURL = 'http://192.168.1.73:3000/api/mail'
   constructor(private http: HttpClient) { }
 
   addEmail(data:any) {
--- src/app/landing/info-section/info-section.component.ts
+++ src/app/landing/info-section/info-section.component.ts
@@ -1,53 +1,65 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import {
+  Component,
+  HostListener,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-info-section',
   templateUrl: './info-section.component.html',
   styleUrls: ['./info-section.component.css'],
   encapsulation: ViewEncapsulation.None,
-
 })
 export class InfoSectionComponent implements OnInit {
-
-
+  public innerWidth: any;
+  public width = false;
+  element: any;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth <= 1100) {
+      this.width = true;
+    } else {
+      this.width = false;
+    }
+  }
 
   images: any = [
-
-   
-
-
-
     {
-      imageURL: '../../../assets/carsole/(n) Managed services 750 H - 2200 W.svg',
-      route: '/manage-services'
+      imageURL:
+        '../../../assets/carsole/(n) Managed services 750 H - 2200 W.svg',
+      route: '/manage-services',
+      imageInMobile: '../../../assets/img/1-Managed service phone- 960 ×700.png',
+      imageInMobile2: '../../../assets/img/2 Managed service phone - 960 × 654.png'
     },
     {
       imageURL: '../../../assets/carsole/(N)IT CONSULTANCY 750 H x 2200 w.svg',
-      route: '/consulting'
+      route: '/consulting',
+      imageInMobile: '../../../assets/img/1-Managed service phone- 960 ×700.png',
+      imageInMobile2: '../../../assets/img/2 Managed service phone - 960 × 654.png'
     },
     {
       imageURL: '../../../assets/carsole/(N)DEV OPS 750 H x 2200 w.svg',
-      route: '/devops'
+      route: '/devops',
+      imageInMobile: '../../../assets/Dia/IT Consultancy 960 ×700 – 1.svg',
+      imageInMobile2: '../../../assets/Dia/IT Consultancy 960 × 494 – 1.svg'
     },
     {
-      imageURL: '../../../assets/carsole/-nn-INFRASTRUCTURE 2200 -W 750 H – 2.svg',
-      route: '/infrastructure'
+      imageURL:
+        '../../../assets/carsole/-nn-INFRASTRUCTURE 2200 -W 750 H – 2.svg',
+      route: '/infrastructure',
+      imageInMobile: '../../../assets/Dia/P-2 SVG TECHNOLOGY 960 × 750 – 1.svg',
+      imageInMobile2: '../../../assets/Dia/P 1 SVG TECHNOLOGY 960 ×700 – 4.svg'
     },
     {
       imageURL: '../../../assets/carsole/(n) Technology 750 H - 2200 W.svg',
-      route: '/technology'
+      route: '/technology',
+      imageInMobile: '../../../assets/img/1-Managed service phone- 960 ×700.png',
+      imageInMobile2: '../../../assets/img/2 Managed service phone - 960 × 654.png'
     },
+  ];
 
-  ]
+  constructor() {}
 
-
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-
-
-  }
+  ngOnInit(): void {}
 }
