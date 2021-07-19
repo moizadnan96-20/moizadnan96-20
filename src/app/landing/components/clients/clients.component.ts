c71a726ef4ed0c0b26a4aed656f8b266111ddba4
moizadnan96-20 <moizadnan96@gmail.com>
2021-07-19 15:55:26 +0500

    changing done

--- src/app/landing/components/clients/clients.component.ts
+++ src/app/landing/components/clients/clients.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-clients',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-
+  public innerWidth: any;
+  public width = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth <= 1100) {
+      this.width = true;
+    } else {
+      this.width = false;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
+    if (window.innerWidth <= 1100) {
+      this.width = true;
+    } else {
+      this.width = false;
+    }
   }
 
 }
