ee5c7a00a7f97c28e8512fc03cda74ef2c665d85
Muhammad Rizwan <muhammad.rizwan@perceptionit.com>
2021-07-19 15:46:00 +0500

    update carsole images web and mobile view, update the anchor menu

--- src/app/landing/info-section/info-section.component.ts
+++ src/app/landing/info-section/info-section.component.ts
@@ -27,34 +27,34 @@ export class InfoSectionComponent implements OnInit {
   images: any = [
     {
       imageURL:
-        '../../../assets/carsole/(n) Managed services 750 H - 2200 W.svg',
+        '../../../assets/carsole new/Managed services  - CAROUSEL 2200 -W 750 H – 2.svg',
       route: '/manage-services',
-      imageInMobile: '../../../assets/carsole in mobile/Managed Services- CAROUSEL - MOBILE 960 W and 700 H- 2.svg',
+      imageInMobile: '../../../assets/carsole new/Managed Services- CAROUSEL - MOBILE 960 W and 750 H- 2 – 1.svg',
       // imageInMobile2: '../../../assets/img/2 Managed service phone - 960 × 654.png'
     },
     {
-      imageURL: '../../../assets/carsole/(N)IT CONSULTANCY 750 H x 2200 w.svg',
+      imageURL: '../../../assets/carsole new/CAROUSEL- IT CONSULTANCY  2200W 750 – 3.svg',
       route: '/consulting',
-      imageInMobile: '../../../assets/carsole in mobile/- IT CONSULTANCY- CAROUSEL- MOBILE 960 W and 700 H -4.svg',
+      imageInMobile: '../../../assets/carsole new/IT CONSULTANCY--CAROUSEL- MOBILE 960 W and 750 H -4 – 1.svg',
       // imageInMobile2: '../../../assets/img/2 Managed service phone - 960 × 654.png'
     },
     {
-      imageURL: '../../../assets/carsole/(N)DEV OPS 750 H x 2200 w.svg',
+      imageURL: '../../../assets/carsole new/DEV. OPS- CAROUSEL 2200 W 750 – 1.svg',
       route: '/devops',
-      imageInMobile: './../../assets/carsole in mobile/DEV. OPS- CAROUSEL- MOBILE 960 W and 700 H  -1.svg',
+      imageInMobile: '../../../assets/carsole new/DEV. OPS- CAROUSEL- MOBILE 960 W and 750 H – 2.svg',
       // imageInMobile2: '../../../assets/carsole in mobile/IT Consultancy 960 × 494 – 1.svg'
     },
     {
       imageURL:
-        '../../../assets/carsole/-nn-INFRASTRUCTURE 2200 -W 750 H – 2.svg',
+        '../../../assets/carsole new/INFRASTRUCTURE - CAROUSEL 2200W  x 750 – 5.svg',
       route: '/infrastructure',
-      imageInMobile: '../../../assets/carsole in mobile/INFRASTRUCTURE - CAROUSEL- MOBILE 960 W and 700 H – 5.svg',
+      imageInMobile: '../../../assets/carsole new/INFRASTRUCTUR- CAROUSEL- MOBILE 960 W and 750 H – 6.svg',
       // imageInMobile2: '../../../assets/carsole in mobile/P 1 SVG TECHNOLOGY 960 ×700 – 4.svg'
     },
     {
-      imageURL: '../../../assets/carsole/(n) Technology 750 H - 2200 W.svg',
+      imageURL: '../../../assets/carsole new/N- TECHNOLOGY- CAROUSEL 2200W 750 – 4.svg',
       route: '/technology',
-      imageInMobile: '../../../assets/carsole in mobile/Technology- CAROUSEL- MOBILE 960 W and 700 H – 3.svg',
+      imageInMobile: '../../../assets/carsole new/TECHNOLOGY-- CAROUSEL- MOBILE 960 W and 750 H – 4.svg',
       // imageInMobile2: '../../../assets/img/2 Managed service phone - 960 × 654.png'
     },
   ];
