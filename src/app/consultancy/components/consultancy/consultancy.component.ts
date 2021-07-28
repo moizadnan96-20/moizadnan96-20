98bf6e872f26e891f6513c3cabab67df1bb7a061
Muhammad Rizwan <muhammad.rizwan@perceptionit.com>
2021-07-28 13:07:44 +0500

    add the animation in the managed service and consultancy page and update the hover in card

--- src/app/consultancy/components/consultancy/consultancy.component.ts
+++ src/app/consultancy/components/consultancy/consultancy.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-consultancy',
   templateUrl: './consultancy.component.html',
@@ -10,6 +10,8 @@ export class ConsultancyComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
+    console.log(AOS.init());
   }
 
 }
--- src/app/manage-services/components/manage-services/info-section/info-section.component.ts
+++ src/app/manage-services/components/manage-services/info-section/info-section.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-info-section',
   templateUrl: './info-section.component.html',
-  styleUrls: ['./info-section.component.css']
+  styleUrls: ['./info-section.component.css'],
 })
 export class InfoSectionComponent implements OnInit {
- @Input() public parentDate=""
-  constructor() { }
+  @Input() public parentDate = '';
+  constructor() {}
 
   ngOnInit(): void {
-
+    {
+      AOS.init();
+    }
+    console.log(AOS);
   }
-  
 }
-
-
